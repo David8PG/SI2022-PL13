b9eda5268e734058e18cc945ec923fe4050e3cb4
uo276569 <uo276569@GI-AN-B7_07.aulasuo.uniovi.es>
2023-03-01 12:10:38 +0100

    Vista Planificar Actividad

---
+++ src/main/java/giis/demo/tkrun/PlanificarActividad.java
@@ -0,0 +1,43 @@
+package giis.demo.tkrun;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class PlanificarActividad {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PlanificarActividad window = new PlanificarActividad();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PlanificarActividad() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
