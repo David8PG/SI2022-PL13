c52aa244b4c4d7b43d2d75a1d96ba1de22ea8ccb
David8PG <uo276569@uniovi.es>
2023-03-01 15:10:27 +0100

    Prototipo Ventana Planificar Actividad

--- src/main/java/giis/demo/tkrun/PlanificarActividad.java
+++ src/main/java/giis/demo/tkrun/PlanificarActividad.java
@@ -3,10 +3,27 @@ package giis.demo.tkrun;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JSpinner;
+import javax.swing.JButton;
+import java.awt.Font;
 
 public class PlanificarActividad {
 
-	private JFrame frame;
+	private JFrame frmPlanificarActividad;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
 
 	/**
 	 * Launch the application.
@@ -16,7 +33,7 @@ public class PlanificarActividad {
 			public void run() {
 				try {
 					PlanificarActividad window = new PlanificarActividad();
-					window.frame.setVisible(true);
+					window.frmPlanificarActividad.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +52,140 @@ public class PlanificarActividad {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmPlanificarActividad = new JFrame();
+		frmPlanificarActividad.setTitle("Planificar Actividad");
+		frmPlanificarActividad.setBounds(100, 100, 372, 577);
+		frmPlanificarActividad.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frmPlanificarActividad.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(84, 12, 241, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Nombre");
+		lblNewLabel.setBounds(21, 14, 56, 17);
+		panel.add(lblNewLabel);
+		
+		JLabel lblDescripcin = new JLabel("Descripción");
+		lblDescripcin.setBounds(21, 42, 56, 17);
+		panel.add(lblDescripcin);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(84, 40, 241, 78);
+		panel.add(textField_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("Instalación");
+		lblNewLabel_1.setBounds(21, 137, 56, 14);
+		panel.add(lblNewLabel_1);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(84, 133, 132, 22);
+		panel.add(comboBox);
+		
+		JLabel lblNewLabel_2 = new JLabel("Deporte");
+		lblNewLabel_2.setBounds(21, 170, 46, 14);
+		panel.add(lblNewLabel_2);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(84, 166, 132, 22);
+		panel.add(comboBox_1);
+		
+		JLabel lblNewLabel_3 = new JLabel("Aforo");
+		lblNewLabel_3.setBounds(21, 207, 46, 14);
+		panel.add(lblNewLabel_3);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(84, 204, 46, 20);
+		panel.add(spinner);
+		
+		JLabel lblNewLabel_4 = new JLabel("Precio");
+		lblNewLabel_4.setBounds(21, 242, 46, 14);
+		panel.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_4_1 = new JLabel("Precio No Socio");
+		lblNewLabel_4_1.setBounds(171, 242, 89, 14);
+		panel.add(lblNewLabel_4_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(84, 240, 56, 17);
+		panel.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(245, 240, 61, 17);
+		panel.add(textField_3);
+		
+		JLabel lblNewLabel_5 = new JLabel("€");
+		lblNewLabel_5.setBounds(142, 242, 46, 14);
+		panel.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_5_1 = new JLabel("€");
+		lblNewLabel_5_1.setBounds(312, 242, 46, 14);
+		panel.add(lblNewLabel_5_1);
+		
+		JLabel lblNewLabel_6 = new JLabel("Periodo Inscripción");
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_6.setBounds(84, 280, 120, 14);
+		panel.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("Fecha Inicio Socios");
+		lblNewLabel_7.setBounds(21, 305, 132, 17);
+		panel.add(lblNewLabel_7);
+		
+		JLabel lblNewLabel_8 = new JLabel("Fecha Fin Socios");
+		lblNewLabel_8.setBounds(21, 338, 103, 14);
+		panel.add(lblNewLabel_8);
+		
+		JLabel lblNewLabel_8_1 = new JLabel("Fecha Fin no Socios");
+		lblNewLabel_8_1.setBounds(21, 374, 103, 14);
+		panel.add(lblNewLabel_8_1);
+		
+		JButton btnNewButton = new JButton("Crear Actividad");
+		btnNewButton.setBounds(96, 504, 120, 23);
+		panel.add(btnNewButton);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(130, 305, 114, 20);
+		panel.add(textField_4);
+		textField_4.setColumns(10);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(130, 335, 114, 20);
+		panel.add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(130, 371, 114, 20);
+		panel.add(textField_6);
+		
+		JLabel lblNewLabel_9 = new JLabel("Duración");
+		lblNewLabel_9.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_9.setBounds(94, 401, 76, 14);
+		panel.add(lblNewLabel_9);
+		
+		JLabel lblNewLabel_10 = new JLabel("Fecha Inicio");
+		lblNewLabel_10.setBounds(21, 424, 68, 14);
+		panel.add(lblNewLabel_10);
+		
+		JLabel lblNewLabel_11 = new JLabel("Fecha Fin");
+		lblNewLabel_11.setBounds(21, 461, 46, 14);
+		panel.add(lblNewLabel_11);
+		
+		textField_7 = new JTextField();
+		textField_7.setBounds(130, 426, 114, 20);
+		panel.add(textField_7);
+		textField_7.setColumns(10);
+		
+		textField_8 = new JTextField();
+		textField_8.setColumns(10);
+		textField_8.setBounds(130, 458, 114, 20);
+		panel.add(textField_8);
 	}
-
 }
