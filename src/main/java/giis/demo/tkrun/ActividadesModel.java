6ece82785917c362e375afb2f1266369f5217d53
jesus <jesus@DESKTOP-G95DKOA>
2023-03-04 23:47:49 +0100

    61 hecha?

--- src/main/java/giis/demo/tkrun/ActividadesModel.java
+++ src/main/java/giis/demo/tkrun/ActividadesModel.java
@@ -1,5 +1,62 @@
 package giis.demo.tkrun;
 
-public class ActividadesModel {
+import java.util.List;
 
+import giis.demo.util.Database;
+
+public class ActividadesModel {
+	private Database bd = new Database();
+
+	// Obtencion del id de actividad
+	public static final String id_actividad = "SELECT id_actividad FROM actividades WHERE nombre=";
+
+	public long getIdActividad(String nombre_actividad) {
+		List<Object[]> lIds_actividad;
+		lIds_actividad = bd.executeQueryArray(id_actividad + "'" + nombre_actividad + "'");
+		return (long) lIds_actividad.get(0)[0];
+	}
+
+	public static final String siguienteUltID = "SELECT MAX(id_actividad) from actividades;";
+
+	public long siguienteIdActividad() {
+		List<Object[]> listaID;
+		listaID = bd.executeQueryArray(siguienteUltID);
+		return (long) listaID.get(0)[0] + 1;
+	}
+
+	public static final String nueva_actividad = "INSERT INTO actividades(id_actividad, nombre, descripcion, aforo, plazas_disponibles, precio_socio, precio_no_socio, fecha_inicio, fecha_fin, deporte , id_instalaciones, id_periodo_inscripciones) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+
+	public void nuevaActividad(String nombre, String descripcion, String aforo, String plazas, String precio_socio,
+			String precio_no_socio, String fecha_inicio, String fecha_fin, String deporte, String id_instalaciones,
+			String id_periodo_inscripciones) {
+		long id;
+		id = siguienteIdActividad();
+		bd.executeUpdate(nueva_actividad, id, nombre, descripcion, aforo, plazas, precio_socio, precio_no_socio,
+				fecha_inicio, fecha_fin, deporte, id_instalaciones, id_periodo_inscripciones);
+	}
+
+	public long nuevaActividadConId(String nombre, String descripcion, String aforo, String plazas, String precio_socio,
+			String precio_no_socio, String fecha_inicio, String fecha_fin, String deporte, String id_instalaciones,
+			String id_periodo_inscripciones) {
+		long id;
+		id = siguienteIdActividad();
+		bd.executeUpdate(nueva_actividad, id, nombre, descripcion, aforo, plazas, precio_socio, precio_no_socio,
+				fecha_inicio, fecha_fin, deporte, id_instalaciones, id_periodo_inscripciones);
+		return id;
+	}
+
+	// Obtención de los nombres de todas las actividades
+	public static final String nombres_actividades = "SELECT nombre FROM actividades";
+
+	public List<Object[]> getNombreActividades() {
+		return bd.executeQueryArray(nombres_actividades);
+	}
+
+	// Listar actividades en un periodo
+	public static final String actividadesPeriodo = "SELECT id_actividad, deporte, plazas_disponibles, fecha_inicio, fecha_fin, aforo, precio_socio, precio_no_socio FROM actividades WHERE fecha_inicio >=";
+
+	public List<Object[]> getActividadesPeriodo(String fechaInicial, String fechaFin) {
+		return bd.executeQueryArray(
+				actividadesPeriodo + "'" + fechaInicial + "'" + " AND fecha_inicio <= '" + fechaFin + "';");
+	}
 }
--- src/main/java/giis/demo/tkrun/SesionesModel.java
+++ src/main/java/giis/demo/tkrun/SesionesModel.java
@@ -1,5 +1,23 @@
 package giis.demo.tkrun;
 
-public class SesionesModel {
+import java.util.List;
 
+import giis.demo.util.Database;
+
+public class SesionesModel {
+	private Database bd = new Database();
+	
+	public static final String siguienteUltID = "SELECT MAX(id_sesion) from sesiones;";
+	public long siguienteIDSesion() {
+		List<Object[]> listaID;
+		listaID = bd.executeQueryArray(siguienteUltID);
+		return (long)listaID.get(0)[0] + 1;
+	}
+	
+	public static final String nueva_sesion = "INSERT INTO sesiones(id_sesion, dia, hora_inicio, hora_fin, id_actividades) VALUES (?, ?, ?, ?, ?);";
+	public void nuevaSesion(String dia, String hora_ini, String hora_fin, long idActividad) {
+		long id;
+		id = siguienteIDSesion();
+		bd.executeUpdate(nueva_sesion, id, dia, hora_ini, hora_fin, idActividad);
+	}
 }
