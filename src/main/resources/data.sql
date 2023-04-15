0b092b4721bd57ceea83749fddae697f5a9120ad
David8PG <uo276569@uniovi.es>
2023-04-15 14:29:46 +0200

    Nuevo data y esquema

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,10 +9,10 @@ DELETE FROM actividades;
 DELETE FROM instalaciones;
 DELETE FROM periodos_inscripcion;
 INSERT INTO clientes(nombre, dni, id_socio, fecha_nacimiento, PagoPendiente, contraseña, cuotaInicial, cuotaReservas, cuotaActividades, telefono, direccion) VALUES
-('Juan Pérez', '11111111A', 1, '1980-05-20', 1, 'contraseña1', 50.00, 10.00, 15.00, '666555444', 'Calle Mayor 1'),
+('Juan Pérez', '11111111A', 1, '1980-05-20', 0, 'contraseña1', 50.00, 10.00, 15.00, '666555444', 'Calle Mayor 1'),
 ('María García', '22222222B', null, null, 0, null, null, null, null, '666444333', null),
 ('Pedro Fernández', '33333333C', 3, '1990-07-03', 0, 'contraseña3', 50.00, 15.00, 25.00, '666333222', 'Plaza España 8'),
-('Sara Gómez', '44444444D', 4, '1995-10-15', 1, 'contraseña4', 0, 17.50, 0, '666222111', 'Avenida Libertad 12'),
+('Sara Gómez', '44444444D', 4, '1995-10-15', 0, 'contraseña4', 0, 17.50, 0, '666222111', 'Avenida Libertad 12'),
 ('Luisa Martínez', '55555555E', 5, '2000-01-01', 0, 'contraseña5', 0, 20.00, 35.00, '666111000', 'Calle Nueva 17'),
 ('Mario Rodríguez', '66666666F', null, null, 0, null, null, null, null, '666000999', null),
 ('Elena Sánchez', '77777777G', 7, '1978-09-25', 1, 'contraseña7', 0, 25.00, 45.00, '666999888', 'Calle Toledo 33'),
@@ -48,12 +48,12 @@ INSERT INTO periodos_inscripcion(id_periodo_inscripcion, nombre_periodo, descrip
 
 
 INSERT INTO actividades(id_actividad, nombre, descripcion, aforo, plazas_disponibles, precio_socio, precio_no_socio, fecha_inicio, fecha_fin, deporte , id_instalaciones, id_periodo_inscripciones) VALUES 
-(1, 'Clase de yoga', 'Clase de yoga para todos los niveles', 20, 20, 5.00, 10.00, '2023-03-01', '2023-04-01', 'Yoga', 3, 3),
+(1, 'Clase de yoga', 'Clase de yoga para todos los niveles', 20, 6, 5.00, 10.00, '2023-03-01', '2023-04-01', 'Yoga', 3, 1),
 (2, 'Partido de fútbol', 'Solteros vs Casados', 22, 22, 0.00, 5.00, '2023-03-02', '2023-03-02', 'Fútbol', 6, 1),
-(3, 'Clase de natación', 'Clase de natación para gente que no sabe nadar', 15, 15, 10.00, 15.00, '2023-03-03', '2023-04-03', 'Natación', 1, 3),
-(4, 'Entrenamiento de boxeo', 'Entrenamiento de boxeo para todos los niveles', 12, 12, 7.00, 12.00, '2023-03-04', '2023-04-04', 'Boxeo', 3, 3),
+(3, 'Clase de natación', 'Clase de natación para gente que no sabe nadar', 15, 15, 10.00, 15.00, '2023-03-03', '2023-04-03', 'Natación', 1, 1),
+(4, 'Entrenamiento de boxeo', 'Entrenamiento de boxeo para todos los niveles', 12, 12, 7.00, 12.00, '2023-03-04', '2023-04-04', 'Boxeo', 3, 1),
 (5, 'Clase de baile', 'Clase de baile para principiantes', 25, 25, 8.00, 13.00, '2023-03-05', '2023-04-05', 'Baile', 3, 1),
-(6, 'Partido pádel', 'Pepe se enfrenta a tí', 4, 0, 8.00, 13.00, '2023-03-05', '2023-04-05', 'Pádel', 4, 3);
+(6, 'Partido pádel', 'Pepe se enfrenta a tí', 4, 0, 8.00, 13.00, '2023-03-05', '2023-04-05', 'Pádel', 4, 1);
 
 
 INSERT INTO inscripciones(id_inscripcion, dni_clientes, id_actividades, fecha) VALUES
@@ -69,8 +69,9 @@ INSERT INTO sesiones(id_sesion, dia, hora_inicio, hora_fin, id_actividades) VALU
 (1, 'lunes', '13:00:00', '15:00:00', '3'), 
 (2, 'martes', '12:00:00', '13:00:00', '2'), 
 (3, 'miércoles', '19:00:00', '21:00:00', '6'),
-(4, 'jueves', '20:00:00', '22:00:00', '4'), 
-(5, 'viernes', '10:00:00', '12:00:00', '1'); 
+(4, 'jueves', '20:00:00', '22:00:00', '1'), 
+(5, 'viernes', '10:00:00', '12:00:00', '1');
+
 
 
 INSERT INTO pagos(id_pago, fecha, dni_clientes, id_inscripciones, id_reservas) VALUES
@@ -80,7 +81,17 @@ INSERT INTO pagos(id_pago, fecha, dni_clientes, id_inscripciones, id_reservas) V
 (4, '2022-02-26', '66666666F', 2, 1),
 (5, '2022-02-25', '44444444D', NULL, 7),
 (6, '2022-03-29', '44444444D', NULL, 6),
-(7, '2022-03-25', '66666666F', 4, 1);
+(7, '2022-03-25', '66666666F', NULL, 1),
+(8, '2022-03-25', '11111111A', 4, 1);
 
-INSERT INTO cola(id_cola, dni_clientes, id_actividades, fecha) VALUES
-(0,NULL,NULL,NULL);
+INSERT INTO cola(id_cola, dni_clientes, id_actividades, fecha,socio) VALUES
+(0,null,null,null,null),
+(1,'11111111A', 1, '2023-04-15 10:00:00', 'sí'),
+(2,'11111111A', 2, '2023-04-15 10:00:00', 'sí'),
+(3,'88888888H', 1, '2023-04-18 11:00:00', 'sí'),
+(4,'55555555E', 1, '2023-04-17 16:00:00', 'sí'),
+(5,'44444444D', 1, '2023-04-20 10:00:00', 'sí'),
+(6,'11111111J', 1, '2023-04-20 10:00:00', 'no'),
+(7,'44444444D', 6, '2023-04-20 10:00:00', 'sí');
+
+
